d9793cc798a82514339c4bdb9a93f2efb372b1ce
wmsiddiqui <wmsiddiqui.6@gmail.com>
2018-03-08 18:17:27 -0600

    Added logic to parital generation when cap is being met

--- tests/unit/blockTests.js
+++ tests/unit/blockTests.js
@@ -9,86 +9,138 @@ var blockSaver = {
 };
 
 describe('block tests', function() {
-	it('should create the block correctly', function() {
-		var id = 1;
+	describe('create tests', function() {
+		it('should create the block correctly', function() {
+			var id = 1;
 
-		var blockInitProperties = {
-			altitude: 10,
-			ether: 20,
-			plasma: 30,
-			matter: 40,
-			cap: 50
-		};
+			var blockInitProperties = {
+				altitude: 10,
+				ether: 20,
+				plasma: 30,
+				matter: 40,
+				cap: 50
+			};
 
-		var testBlock = new Block(id, blockInitProperties, blockSaver);
+			var testBlock = new Block(id, blockInitProperties, blockSaver);
 
-		assert.equal(testBlock._ether, blockInitProperties.ether);
-		assert.equal(testBlock._plasma, blockInitProperties.plasma);
-		assert.equal(testBlock._matter, blockInitProperties.matter);
-		assert.equal(testBlock._altitude, blockInitProperties.altitude);
-		assert.equal(testBlock._cap, blockInitProperties.cap);
-		assert.equal(testBlock.getId(), id);
+			assert.equal(testBlock._ether, blockInitProperties.ether);
+			assert.equal(testBlock._plasma, blockInitProperties.plasma);
+			assert.equal(testBlock._matter, blockInitProperties.matter);
+			assert.equal(testBlock._altitude, blockInitProperties.altitude);
+			assert.equal(testBlock._cap, blockInitProperties.cap);
+			assert.equal(testBlock.getId(), id);
+		});
 	});
+	describe('generate resources tests', function() {
+		it('should generate new resources correctly', function() {
+			var blockInitProperties = {
+				altitude: 10,
+				ether: 20,
+				plasma: 30,
+				matter: 40,
+				cap: 100
+			};
 
-	it('should generate new resources correctly', function() {
-		var blockInitProperties = {
-			altitude: 10,
-			ether: 20,
-			plasma: 30,
-			matter: 40,
-			cap: 100
-		};
+			var etherIncrement = 1;
+			var plasmaIncrement = 2;
+			var matterIncrement = 3;
 
-		var etherIncrement = 1;
-		var plasmaIncrement = 2;
-		var matterIncrement = 3;
+			var testBlock = new Block(1, blockInitProperties, blockSaver);
 
-		var testBlock = new Block(1, blockInitProperties, blockSaver);
+			testBlock.generateResources(etherIncrement, plasmaIncrement, matterIncrement);
 
-		testBlock.generateResources(etherIncrement, plasmaIncrement, matterIncrement);
+			assert.equal(testBlock._ether, blockInitProperties.ether + etherIncrement);
+			assert.equal(testBlock._plasma, blockInitProperties.plasma + plasmaIncrement);
+			assert.equal(testBlock._matter, blockInitProperties.matter + matterIncrement);
+		});
 
-		assert.equal(testBlock._ether, blockInitProperties.ether + etherIncrement);
-		assert.equal(testBlock._plasma, blockInitProperties.plasma + plasmaIncrement);
-		assert.equal(testBlock._matter, blockInitProperties.matter + matterIncrement);
+		it('should not generate new resources if cap is met', function() {
+			var blockInitProperties = {
+				altitude: 10,
+				ether: 10,
+				plasma: 10,
+				matter: 10,
+				cap: 30
+			};
+
+			var etherIncrement = 1;
+			var plasmaIncrement = 2;
+			var matterIncrement = 3;
+
+			var testBlock = new Block(1, blockInitProperties, blockSaver);
+
+			testBlock.generateResources(etherIncrement, plasmaIncrement, matterIncrement);
+
+			assert.equal(testBlock._ether, blockInitProperties.ether);
+			assert.equal(testBlock._plasma, blockInitProperties.plasma);
+			assert.equal(testBlock._matter, blockInitProperties.matter);
+		});
+
+		it('should generate new resources in proportion if total added exceeds cap', function() {
+			var blockInitProperties = {
+				altitude: 10,
+				ether: 0,
+				plasma: 0,
+				matter: 0,
+				cap: 100
+			};
+
+			var etherIncrement = 100;
+			var plasmaIncrement = 40;
+			var matterIncrement = 60;
+
+			var testBlock = new Block(1, blockInitProperties, blockSaver);
+
+			testBlock.generateResources(etherIncrement, plasmaIncrement, matterIncrement);
+
+			var expectedEther = 50;
+			var expectedPlasma = 20;
+			var expectedMatter = 30;
+
+			assert.equal(testBlock._ether, expectedEther);
+			assert.equal(testBlock._plasma, expectedPlasma);
+			assert.equal(testBlock._matter, expectedMatter);
+		});
 	});
+	describe('consume tests', function() {
+		it('should consume resources and return true if there are enough resources', function() {
+			var blockInitProperties = {
+				altitude: 10,
+				ether: 20,
+				plasma: 30,
+				matter: 40,
+				cap: 50
+			};
 
-	it('should consume resources and return true if there are enough resources', function() {
-		var blockInitProperties = {
-			altitude: 10,
-			ether: 20,
-			plasma: 30,
-			matter: 40,
-			cap: 50
-		};
+			var etherDecrement = 10;
+			var plasmaDecrement = 2;
+			var matterDecrement = 3;
 
-		var etherDecrement = 10;
-		var plasmaDecrement = 2;
-		var matterDecrement = 3;
+			var testBlock = new Block(1, blockInitProperties, blockSaver);
 
-		var testBlock = new Block(1, blockInitProperties, blockSaver);
+			var consumeResult = testBlock.consumeResources(etherDecrement, plasmaDecrement, matterDecrement);
 
-		var consumeResult = testBlock.consumeResources(etherDecrement, plasmaDecrement, matterDecrement);
+			assert.isTrue(consumeResult);
+		});
 
-		assert.isTrue(consumeResult);
-	});
+		it('should not consume resources and return false if there are enough resources', function() {
+			var blockInitProperties = {
+				altitude: 10,
+				ether: 2,
+				plasma: 30,
+				matter: 40,
+				cap: 50
+			};
 
-	it('should not consume resources and return false if there are enough resources', function() {
-		var blockInitProperties = {
-			altitude: 10,
-			ether: 2,
-			plasma: 30,
-			matter: 40,
-			cap: 50
-		};
+			var etherDecrement = 10;
+			var plasmaDecrement = 2;
+			var matterDecrement = 3;
 
-		var etherDecrement = 10;
-		var plasmaDecrement = 2;
-		var matterDecrement = 3;
+			var testBlock = new Block(1, blockInitProperties, blockSaver);
 
-		var testBlock = new Block(1, blockInitProperties, blockSaver);
+			var consumeResult = testBlock.consumeResources(etherDecrement, plasmaDecrement, matterDecrement);
 
-		var consumeResult = testBlock.consumeResources(etherDecrement, plasmaDecrement, matterDecrement);
-
-		assert.isFalse(consumeResult);
+			assert.isFalse(consumeResult);
+		});
 	});
 });
--- worldGenerator/models/block.js
+++ worldGenerator/models/block.js
@@ -19,25 +19,33 @@ module.exports = class Block {
 		this._save();
 	}
 
-	generateResources(ether, plasma, matter) {
-		let generateTotal = ether + plasma + matter;
-		let existingTotal = this._ether + this._plasma + this._matter;
+	generateResources(etherAdded, plasmaAdded, matterAdded) {
+		var generateTotal = etherAdded + plasmaAdded + matterAdded;
+		var existingTotal = this._ether + this._plasma + this._matter;
 		if (generateTotal + existingTotal < this._cap) {
-			this._ether += ether;
-			this._plasma += plasma;
-			this._matter += matter;
+			this._ether += etherAdded;
+			this._plasma += plasmaAdded;
+			this._matter += matterAdded;
 		} else if (existingTotal < this._cap) {
-			//TODO: somehow distribute amounts?
+			var remainingCapacity = this._cap - existingTotal;
+			var ratio = remainingCapacity / generateTotal;
+			var scaledEther = etherAdded * ratio;
+			var scaledPlasma = plasmaAdded * ratio;
+			var scaledMatter = matterAdded * ratio;
+
+			this._ether += Math.trunc(scaledEther);
+			this._plasma += Math.trunc(scaledPlasma);
+			this._matter += Math.trunc(scaledMatter);
 		}
 
 		this._save();
 	}
 
-	consumeResources(ether, plasma, matter) {
-		if (ether <= this._ether && plasma <= this._plasma && matter <= this._matter) {
-			this._ether -= ether;
-			this._plasma -= plasma;
-			this._matter -= matter;
+	consumeResources(etherUsed, plasmaUsed, matterUsed) {
+		if (etherUsed <= this._ether && plasmaUsed <= this._plasma && matterUsed <= this._matter) {
+			this._ether -= etherUsed;
+			this._plasma -= plasmaUsed;
+			this._matter -= matterUsed;
 			this._save();
 			return true;
 		}
