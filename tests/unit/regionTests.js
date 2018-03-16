2d25416e0373fd381053d541c41fa97b728d3d1c
wmsiddiqui <wmsiddiqui.6@gmail.com>
2018-03-16 08:00:18 -0500

    Added constructor for region with tests

--- tests/unit/regionTests.js
+++ tests/unit/regionTests.js
@@ -1,10 +1,34 @@
 var chai = require('chai');
 var assert = chai.assert;
-var worldGenerator = require('../../worldGenerator/models/region');
+var Region = require('../../worldGenerator/models/region');
 
-xdescribe('region', function() {
-	it('should correctly create the first block', function() {
-		var generator = worldGenerator.getWorldGenerator();
-		generator.createBlock;
+describe('region', function() {
+	describe('create region', function() {
+		it('should create region', function() {
+			var region = new Region(1, 11);
+			assert.equal(1, region._id);
+			assert.equal(11, region._regionSize);
+		});
+		it('should throw error if region is negative', function() {
+			assert.throws(function() {
+				new Region(1, -1);
+			});
+		});
+		it('should throw error if region is 0', function() {
+			assert.throws(function() {
+				new Region(1, 0);
+			});
+		});
+		it('should throw error if region is over 200', function() {
+			assert.throws(function() {
+				new Region(1, 201);
+			});
+		});
+	});
+	describe('create first block', function() {
+		xit('should correctly create the first block', function() {
+			var generator = worldGenerator.getWorldGenerator();
+			generator.createBlock;
+		});
 	});
 });
--- worldGenerator/models/region.js
+++ worldGenerator/models/region.js
@@ -1,21 +1,24 @@
 var Block = require('./block');
 var blockTypes = require('./blockTypes');
+var numberChecker = require('../utils/numberChecker');
 
 var worldSize = 11;
 
 module.exports = class Region {
-	constructor(id, saveClient) {
+	constructor(id, regionSize, saveClient) {
 		this._id = id;
 		this._saveClient = saveClient;
 
-		this._worldMap = [ [] ];
+		if (!numberChecker.isPositiveWholeNumber(regionSize) || regionSize <= 0 || regionSize > 200) {
+			throw new Error('Region Size is invalid');
+		}
+
+		this._regionSize = regionSize;
+		this._regionMap = [ [] ];
 		this._availableAreas = [];
 		this._remainingCapacity = worldSize * worldSize;
 	}
 
-	getRemainingCapacity() {
-		return this._remainingCapacity;
-	}
 	createBlock() {
 		if (remainingCapacity == worldSize * worldSize && availableAreas.length == 0) {
 			//First Block
