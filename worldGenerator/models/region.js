a3eaa0d06d90af0d62d6144195a3ce4e01bfac1f
wmsiddiqui <wmsiddiqui.6@gmail.com>
2018-03-16 20:53:35 -0500

    refactored how coordinates are stored

--- worldGenerator/models/region.js
+++ worldGenerator/models/region.js
@@ -24,21 +24,22 @@ module.exports = class Region {
 
 	createBlock() {
 		var block;
+		var coordinates;
 		if (this._remainingCapacity == this._regionSize * this._regionSize && this._availableAreas.length == 0) {
 			//First Block
 			var generatedBlockType = getRandomBlockType();
-			var coordinate = Math.ceil(this._regionSize / 2);
+			var coordinateNumber = Math.ceil(this._regionSize / 2);
 			var blockInitProperties = {
 				cap: 1,
 				altitude: 10,
 				blockType: generatedBlockType
 			};
 			block = new Block(this._id + '-' + 1, blockInitProperties, this._saveClient);
-			this._regionMap[coordinate - 1][coordinate - 1] = block;
+			coordinates = [ coordinateNumber - 1, coordinateNumber - 1 ];
 		}
 
 		//check to see if neighbors are inside region
-		//if()
+		this._regionMap[coordinates[0]][coordinates[1]] = block;
 		this._remainingCapacity--;
 		return block;
 	}
