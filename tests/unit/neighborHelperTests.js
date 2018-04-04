416054c38df1994bd26095eb3d7e11fe320de9c1
wmsiddiqui <wmsiddiqui.6@gmail.com>
2018-04-04 18:15:21 -0500

    Added corner cases to tests

--- tests/unit/neighborHelperTests.js
+++ tests/unit/neighborHelperTests.js
@@ -28,5 +28,24 @@ describe('neighborHelper', function() {
 			var neighbors = neighborHelper.getNeighborBlocks([ 1, 1 ], regionMap);
 			assert.equal(neighbors.length, 0);
 		});
+
+		it('should return a list of all the neighbors of the corner blocks', function() {
+			regionMap = [];
+			for (var i = 0; i < 3; i++) {
+				regionMap[i] = [ {}, {}, {} ];
+			}
+
+			var bottomLeftNeighbors = neighborHelper.getNeighborBlocks([ 0, 0 ], regionMap);
+			assert.equal(bottomLeftNeighbors.length, 2);
+
+			var topLeftNeighbors = neighborHelper.getNeighborBlocks([ 0, 2 ], regionMap);
+			assert.equal(topLeftNeighbors.length, 2);
+
+			var bottomRightNeighbors = neighborHelper.getNeighborBlocks([ 2, 0 ], regionMap);
+			assert.equal(bottomRightNeighbors.length, 2);
+
+			var topRightNeighbors = neighborHelper.getNeighborBlocks([ 2, 2 ], regionMap);
+			assert.equal(topRightNeighbors.length, 2);
+		});
 	});
 });
